3002b0017f910f891b9a8ee7621cfc941fcf3ec4
shankaranand12 <shankaranand12@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-26 17:21:49 +0000

    version 1.2 assets folder
    git-svn-id: https://plugins.svn.wordpress.org/multi-image-widget/trunk@1167864 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- assets/js/miw_frontend_custom.js
+++ assets/js/miw_frontend_custom.js
@@ -10,7 +10,8 @@ jQuery(document).ready(function($) {
         navigation: true, // Show next and prev buttons
         slideSpeed: 300,
         paginationSpeed: 400,
-        singleItem: true
+        singleItem: true,
+        autoPlay: 3000
 
 // "singleItem:true" is a shortcut for:
 // items : 1,
